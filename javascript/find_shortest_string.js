ef381e692b4b599d02fc1e5cecefbc9de86d987b
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-05-26 01:02:22 -0500

    Complete challenge

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,12 @@
 function findShortestString(arr) {
-  // type your code here
+  let shortest_string = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    const length = Math.min(shortest_string.length, arr[i].length);
+    if (length === arr[i].length && length !== shortest_string.length) {
+      shortest_string = arr[i];
+    }
+  }
+  return shortest_string;
 }
 
 if (require.main === module) {
@@ -18,6 +25,25 @@ if (require.main === module) {
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
   // BENCHMARK HERE
+  const small_input = ['asdf','sd','d'];
+  const large_input = [];
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 100; i++) {
+    const random_string_length = Number.parseInt(Math.random() * 40);
+    let string = "";
+    for (let j = 0; j < random_string_length; j++) {
+      string += chars.charAt(Math.floor(Math.random() * chars));
+    }
+    large_input.push(string);
+  }
+  const start = performance.now();
+  for (let i = 0; i < 1000; i++) {
+    findShortestString(small_input);
+    findShortestString(large_input);
+  }
+  const end = performance.now();
+  const runTime = (end - start) / 2000;
+  console.log("Run time: ", runTime);
 }
 
 module.exports = findShortestString;
